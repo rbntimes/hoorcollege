3beee568c3d1fb3e2a88a5ca9575de098bbd37d8
Robin van Leeuwen <robinvanleeuwen1995@hotmail.com>
2019-11-12 11:25:21 +0100

    final

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
@@ -6,7 +10,8 @@ module.exports = {
       "/": { page: "/" }
     };
   },
-  assetPrefix: !debug ? "/hoorcollege" : "",
+  //assetPrefix: '',
+  assetPrefix: !debug ? "/hoorcollege/" : "",
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
@@ -20,11 +25,11 @@ module.exports = {
     // Important: return the modified config
     return config;
   } /*,
-webpackDevMiddleware: (config) => {
-  // Perform customizations to webpack dev middleware config
-  // console.log('webpackDevMiddleware');
-  // console.log(config);
-  // Important: return the modified config
-  return config
-}, */
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // console.log('webpackDevMiddleware');
+    // console.log(config);
+    // Important: return the modified config
+    return config
+  }, */
 };
